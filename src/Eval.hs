55ec817fbb222c7653534448536adc95c21a7327
Brian Shu <littlebubu.shu@gmail.com>
2021-06-26 14:17:30 -0400

    fixed everything

--- src/Eval.hs
+++ src/Eval.hs
@@ -8,10 +8,8 @@ import AST.Expr
 import Control.Monad.Reader
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Maybe
 import Data.Text (Text)
 import qualified Data.Text.Lazy.Builder as TLB
-import Debug.Trace (trace)
 import Poly.Pretty
 import Prettyprinter (Pretty (pretty))
 import TextShow
@@ -21,9 +19,11 @@ type TermEnv = Map Text Value
 emptyTermEnv :: TermEnv
 emptyTermEnv = Map.empty
 
-type Eval t = (ReaderT TermEnv Maybe) t
+-- for some reason when there is maybe and we do fromMaybe it will lag because things are not lazy,
+-- does this have to do with monadfail?
+type Eval t = (Reader TermEnv) t
 
-type MonadEval m = (MonadReader TermEnv m, MonadFail m)
+type MonadEval m = (MonadReader TermEnv m)
 
 data Value
   = VInt Integer
@@ -43,32 +43,24 @@ instance PP Value where
 instance TextShow Value where
   showb v = TLB.fromText $ ppr v
 
-evalExpr :: Expr -> Maybe Value
+evalExpr :: Expr -> Value
 evalExpr e = runEval emptyTermEnv (eval e)
 
-errMsg :: String
-errMsg = "IMPOSSIBLE: a pattern probably failed to match. The expression should have been type checked before passing it to eval"
-
-runEval :: TermEnv -> Eval Value -> Maybe Value
-runEval env m = runReaderT m env
-
-dbg x = trace (show x) x
+runEval :: TermEnv -> Eval Value -> Value
+runEval env m = runReader m env
 
 eval :: MonadEval m => Expr -> m Value
 eval expr = case expr of
   Lit l -> return $ evalLit l
-  -- Var x -> asks (Map.! x)
-  Var x -> do
-    env <- ask
-    let Just v = Map.lookup x env
-    return v
+  Var x -> asks (Map.! x)
   Bin op a b -> do
-    VInt a' <- eval a
-    VInt b' <- eval b
+    ~(VInt a') <- eval a
+    ~(VInt b') <- eval b
     return $ binOp op a' b'
   Lam x body -> asks (VClosure x body)
   App fun arg -> do
-    VClosure x body clo <- eval fun
+    ~(VClosure x body clo) <- eval fun
+    -- forcing argv to be evaluated will cause everything to hang when combining this with fix
     argv <- eval arg
     let nenv = Map.insert x argv clo
     -- local (\_ -> Map.insert x argv clo) (eval body)
@@ -77,17 +69,12 @@ eval expr = case expr of
     v <- eval e
     local (Map.insert x v) (eval body)
   If cond tr fl -> do
-    VBool br <- eval cond
+    ~(VBool br) <- eval cond
     if br
       then eval tr
       else eval fl
-  Fix e -> do
-    -- let !e' = dbg e
-    -- !e'' <- eval (App e (Fix e))
-    eval (App e (Fix e))
-    -- return $ dbg e''
-
--- return $ VStr "not yet implemented"
+  -- this is infinite but it doesn't matter because everything is lazy
+  Fix e -> eval (App e (Fix e))
 
 evalLit :: Lit -> Value
 evalLit (LInt i) = VInt i
@@ -102,6 +89,4 @@ binOp op i i' = case op of
   Add -> VInt $ i + i'
   Sub -> VInt $ i - i'
   Mul -> VInt $ i * i'
-
--- Mul -> VInt $ i / i'
--- Eql -> VBool $ i == i
+  Div -> VInt $ quot i i'
